bc5206a74ff2d14aaec55f7593bb96f542c2bfb8
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-14 17:19:51 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 22, "Male", "Blue","Happy", True);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 24, "Male", "Yellow","Angry", True);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 22, "Male", "Blue","Happy", False);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 22, "Male", "Blue","Happy", True);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 22, "Male", "Blue","Happy", True);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 22, "Male", "Blue","Happy", True);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 22, "Male", "Blue","Happy", True);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 22, "Male", "Blue","Happy", True);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 22, "Male", "Blue","Happy", True);
+
+
